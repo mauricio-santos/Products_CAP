018e092c61c8af92a95c849da5ad8fc24ad6dd2d
Mauricio Vieira dos Santos <mauricio.vieiradossantos@mosolf.de>
2025-02-14 13:37:16 +0000

    CQL - On READ Filtros

--- srv/orders.js
+++ srv/orders.js
@@ -5,6 +5,11 @@ module.exports = (srv) => {
 
     // %%%%%%%%%%%%% READ %%%%%%%%%%%%%
     srv.on("READ", "GetOrders", async (request) => {
+
+        if (request.data.ClientEmail !== undefined) {
+            return await SELECT.from`de.training.Orders`
+                .where`ClientEmail = ${request.data.ClientEmail}`;
+        }
         return await SELECT.from(Orders);
     });
 };
