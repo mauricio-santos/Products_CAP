52046f27af22042d3540386b83b63e88c89f38a9
Mauricio Vieira dos Santos <mauricio.vieiradossantos@mosolf.de>
2025-02-14 14:34:21 +0000

    CQL - On CREATE

--- srv/orders.cds
+++ srv/orders.cds
@@ -2,4 +2,5 @@ using de.training as training from '../db/training';
 
 service ManageOrders {
     entity GetOrders as projection on training.Orders;
+    entity CreateOrder as projection on training.Orders;
 };
--- srv/orders.js
+++ srv/orders.js
@@ -1,4 +1,5 @@
 const cds = require("@sap/cds");
+const { request } = require("express");
 const { Orders } = cds.entities("de.training");
 
 module.exports = (srv) => {
@@ -15,5 +16,37 @@ module.exports = (srv) => {
 
     srv.after("READ", "GetOrders", (data) => { //Convertendo todos os valores de Reviewed para true
         return data.map(order => order.Reviewed = true);
+    });
+
+    // %%%%%%%%%%%%% CREATE %%%%%%%%%%%%%
+    srv.on("CREATE", "CreateOrder", async (req) => {
+        const body = {
+            ClientEmail : req.data.ClientEmail,
+            FirstName : req.data.FirstName,
+            LastName : req.data.LastName,
+            CreatedOn : req.data.CreatedOn,
+            Reviewed : req.data.Reviewed,
+            Approved : req.data.Approved
+        };
+
+        const result = await cds.transaction(req)
+            .run( INSERT.into(Orders).entries(body))
+            .then((resolve, reject) => {
+                console.log("Resolve", resolve);
+                console.log("Reject", reject);
+
+                if (resolve) {
+                    return req.data;
+                }else {
+                    req.error(409, "Record Not Inserted");
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+                req.error(err.code, err.message);
+            });
+            console.log("Result", result);
+            
+        return result;
     });
 };
