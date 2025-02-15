789766f6b5f0ecfd68d2388782505d9f67ecc302
Mauricio Vieira dos Santos <mauricio.vieiradossantos@mosolf.de>
2025-02-15 15:01:00 +0000

    OData Adapter Proxy - Utilizando OData V2

--- srv/server.js
+++ srv/server.js
@@ -1,8 +1,10 @@
 // %%%%%%%%%%%%% BOOTSTRAP %%%%%%%%%%%%%
 const cds = require("@sap/cds");
 const cors = require("cors"); //npm i cors
+const cov2ap = require("@sap/cds-odata-v2-adapter-proxy"); // npm i @sap/cds-odata-v2-adapter-proxy
 
 cds.on("bootstrap", (app) => {
+    app.use(cov2ap()); // Usando OData V2
     app.use(cors()); //Permite realizar petições a serviços externos
     app.get("/alive", (request, response) => {
         response.status(200).send("Server is Alive");
