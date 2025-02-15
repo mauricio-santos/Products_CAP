b7e90668dfb3ea5a6d213953bada748085932ffa
Mauricio Vieira dos Santos <mauricio.vieiradossantos@mosolf.de>
2025-02-15 11:27:14 +0000

    CORS

--- srv/server.js
+++ srv/server.js
@@ -1,7 +1,9 @@
 // %%%%%%%%%%%%% BOOTSTRAP %%%%%%%%%%%%%
 const cds = require("@sap/cds");
+const cors = require("cors"); //npm i cors
 
 cds.on("bootstrap", (app) => {
+    app.use(cors()); //Permite realizar petições a serviços externos
     app.get("/alive", (request, response) => {
         response.status(200).send("Server is Alive");
     });
